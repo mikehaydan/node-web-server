0d0a3580f6628fb88fc354d610ad6c17b1200c0e
Mike Haydan <misha.gajdan@gmail.com>
2018-09-03 15:41:21 +0300

    update projects title

--- server.js
+++ server.js
@@ -42,7 +42,8 @@ app.get('/about', (req, res) => {
 
 app.get('/projects', (req, res) => {
   res.render('projects.hbs', {
-    pageTitle: 'Projects'
+    pageTitle: 'Projects',
+    projectsTitle: 'Projects page'
   });
 });
 
